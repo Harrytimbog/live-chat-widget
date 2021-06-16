b1855bd07b095e7009edc3f7cafc17d9bdd938b8
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-06-16 21:17:33 +0100

    Create chat and chatroom Controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'chats/index'
-  get 'chats/new'
-  get 'chats/create'
-  get 'chats/show'
-  get 'chatrooms/index'
-  get 'chatrooms/new'
-  get 'chatrooms/create'
-  get 'chatrooms/show'
-  root to: 'pages#home'
+  resources :chatrooms
+  resources :chats
+  get '/dashboard', to: 'chats#index'
+  root 'chatrooms#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
